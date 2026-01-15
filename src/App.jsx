c940427b7092c54ce30875d698b08aa78da7bd60
Rahul-krishh <rahulmykris@gmail.com>
2026-01-15 23:47:51 +0530

    feat : add component Link for hyperlinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes , Route } from 'react-router-dom'
 import { HomePage } from './assets/pages/HomePage'
 import { CheckoutPage } from './assets/pages/CheckoutPage'
+import { OrderPage } from './assets/pages/OrderPage'
+import { TrackingPage } from './assets/pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         {/* <Route path='/' element={ <HomePage/> }></Route> */}
         <Route index element={ <HomePage /> }></Route>
         <Route path='checkout' element={ <CheckoutPage/> }></Route>
+        <Route path='orders' element={ <OrderPage/> }></Route>
+        <Route path='track' element={ <TrackingPage/> }></Route>
       </Routes>
     </>
   )
